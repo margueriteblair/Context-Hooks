272a6cdf354daf5d99c90fbb0b98c23986a9efb3
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-26 12:10:56 -0400

    two components for our reading list

---
+++ context-app/src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <div>
+           <nav>
+               <h1>Context App</h1>
+               <ul>
+                   <li>Home</li>
+                   <li>About</li>
+                   <li>Contact</li>
+               </ul>
+            </nav> 
+        </div>
+    )
+}
