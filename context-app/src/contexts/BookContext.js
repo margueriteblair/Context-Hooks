248f4d18042c0456ea20b9af17ff6d4d75848abe
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-27 12:20:23 -0400

    converted state functions into a reducer function

--- context-app/src/contexts/BookContext.js
+++ context-app/src/contexts/BookContext.js
@@ -1,26 +1,27 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useReducer } from 'react'
 import uuid from 'uuid/v1'
+import { bookReducer } from '../Reducers/bookReducer';
 
 export const BookContext = createContext();
 
 export default function BookContextProvider(props) {
-    const [books, setBooks] = useState([
+    const [books, dispatch] = useReducer(bookReducer, [
         {title: 'A Clockwork Orange', author: "Anthony Burgess", id: 1},
         {title: 'A Hero of Ages', author: "Margie Blair", id: 2},
         {title: 'The Jungle', author: "Upton Sinclair", id: 3},
         {title: 'Open Veins of Latin America', author: "Eduardo Galileano", id: 4}
     
     ])
-    const addBook = (title, author) => {
-        setBooks([...books, {title: title, author: author, id: uuid()}])
-    }
+    // const addBook = (title, author) => {
+    //     setBooks([...books, {title: title, author: author, id: uuid()}])
+    // }
 
-    const removeBook = (id)  => {
-        setBooks(books.filter(book => book.id !== id))
-    }
+    // const removeBook = (id)  => {
+    //     setBooks(books.filter(book => book.id !== id))
+    // }
 
   return (
-    <BookContext.Provider value={{books, addBook, removeBook}}>
+    <BookContext.Provider value={{books, dispatch}}>
       {props.children}
     </BookContext.Provider>
   )
